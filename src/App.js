cc5a599a5bcae3cb38aab33a2700f08981201943
Galiov <galilea.valle@gmail.com>
2019-05-29 11:16:19 -0500

    Se agregó componente lector de QR

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Test from './Components/Qr';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+    	<Test/>
     </div>
   );
 }
