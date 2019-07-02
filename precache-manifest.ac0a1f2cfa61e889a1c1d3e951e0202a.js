46b0584c823c3e691f2d2873ec0af2d3363ba457
Martha Navarro <tita1089@gmail.com>
2019-07-02 10:43:06 -0500

    Updates

---
+++ precache-manifest.ac0a1f2cfa61e889a1c1d3e951e0202a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1de9fbd89632fc1aacdcaab24db6cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844e6a7537912d8063d0",
+    "url": "/static/css/main.5841662c.chunk.css"
+  },
+  {
+    "revision": "464dcd41f8bcd6b36c6b",
+    "url": "/static/js/2.9b438469.chunk.js"
+  },
+  {
+    "revision": "844e6a7537912d8063d0",
+    "url": "/static/js/main.d0cb90a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
